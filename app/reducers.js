a415dbcc09079e32dabbc1edf9a715cd05d30a4a
Denis Moiseev <isdenmois@gmail.com>
2016-12-24 11:41:50 +0000

    Update boilr

--- app/reducers.js
+++ app/reducers.js
@@ -7,7 +7,7 @@ import { fromJS } from 'immutable';
 import { combineReducers } from 'redux-immutable';
 import { LOCATION_CHANGE } from 'react-router-redux';
 
-import globalReducer from './containers/App/reducer';
+import globalReducer from 'containers/App/reducer';
 
 /*
  * routeReducer
@@ -19,30 +19,31 @@ import globalReducer from './containers/App/reducer';
 
 // Initial routing state
 const routeInitialState = fromJS({
-    locationBeforeTransitions: null,
+  locationBeforeTransitions: null,
 });
 
 /**
  * Merge route into the global application state
  */
 function routeReducer(state = routeInitialState, action) {
-    switch (action.type) {
-        /* istanbul ignore next */
-        case LOCATION_CHANGE:
-            return state.merge({
-                locationBeforeTransitions: action.payload,
-            });
-        default:
-            return state;
-    }
+  switch (action.type) {
+    /* istanbul ignore next */
+    case LOCATION_CHANGE:
+      return state.merge({
+        locationBeforeTransitions: action.payload,
+      });
+    default:
+      return state;
+  }
 }
 
 /**
  * Creates the main reducer with the asynchronously loaded ones
  */
 export default function createReducer(asyncReducers) {
-    return combineReducers({
-        route: routeReducer,
-        global: globalReducer,
-    });
+  return combineReducers({
+    route: routeReducer,
+    global: globalReducer,
+    ...asyncReducers,
+  });
 }
--- app/routes.js
+++ app/routes.js
@@ -1,40 +1,39 @@
-// App routes.
+// These are the pages you can go to.
+// They are all wrapped in the App component, which should contain the navbar etc
+// See http://blog.mxstbr.com/2016/01/react-apps-with-pages for more information
+// about the code splitting business
+import { getAsyncInjectors } from './utils/asyncInjectors';
 
 const errorLoading = (err) => {
-    console.error('Dynamic page loading failed', err);
+  console.error('Dynamic page loading failed', err); // eslint-disable-line no-console
 };
 
 const loadModule = (cb) => (componentModule) => {
-    cb(null, componentModule.default);
+  cb(null, componentModule.default);
 };
 
 export default function createRoutes(store) {
-    return [
-        {
-            path: '/',
-            name: 'home',
-            getComponent(nextState, cb) {
-                const importModules = Promise.all([
-                    System.import('./containers/HomePage'),
-                ]);
+  // create reusable async injectors using getAsyncInjectors factory
+  const { injectReducer, injectSagas } = getAsyncInjectors(store);
 
-                const renderRoute = loadModule(cb);
-
-                importModules.then(([component]) => {
-                    renderRoute(component);
-                });
-
-                importModules.catch(errorLoading);
-            },
-        },
-        {
-            path: '*',
-            name: 'notfound',
-            getComponent(nextState, cb) {
-                System.import('./containers/NotFoundPage')
-                    .then(loadModule(cb))
-                    .catch(errorLoading);
-            },
-        },
-    ];
+  return [
+    {
+      path: '/',
+      name: 'home',
+      getComponent(nextState, cb) {
+        System.import('containers/HomePage')
+          .then(loadModule(cb))
+          .catch(errorLoading);
+      },
+    },
+    {
+      path: '*',
+      name: 'notfound',
+      getComponent(nextState, cb) {
+        System.import('containers/NotFoundPage')
+          .then(loadModule(cb))
+          .catch(errorLoading);
+      },
+    },
+  ];
 }
