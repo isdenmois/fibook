46b8160255c3d0ab44a37e682195fce9a9a6f8a7
Denis Moiseev <isdenmois@gmail.com>
2016-12-31 18:28:10 +0000

    Fix tests

--- src/components/FileInput/FileInput.test.js
+++ src/components/FileInput/FileInput.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import FileImport from './FileInput';
+import FileInput from './FileInput';
 
 describe('<FileInput />', () => {
     it('should render without errors', () => {
         const fn = jest.fn();
-        const file = shallow(<FileImport name="test" onFileSelect={fn} />);
+        const file = shallow(
+            <FileInput
+                name="test"
+                onFileSelect={fn}
+            >
+                test
+            </FileInput>
+        );
         expect(file).toBeDefined();
     });
 });
--- src/utils/request.test.js
+++ src/utils/request.test.js
@@ -1,1 +1,16 @@
 import request from './request';
+
+describe('Request', () => {
+    it('should work', () => {
+        const toJSON = jest.fn();
+        global.fetch = jest.fn(() => Promise.resolve({
+            status: 200,
+            json: toJSON,
+        }));
+
+        return request('test', {}).then(() => {
+            expect(global.fetch).toHaveBeenCalledWith('test', {});
+            expect(toJSON).toHaveBeenCalled();
+        });
+    });
+});
