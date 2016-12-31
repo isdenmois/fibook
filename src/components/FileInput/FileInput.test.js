d2ff7264ee8f5c389607c42c70a8ec64b7cc6b33
Denis Moiseev <isdenmois@gmail.com>
2016-12-31 18:47:58 +0000

    Update FileInput test

--- src/components/FileInput/FileInput.test.js
+++ src/components/FileInput/FileInput.test.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import FileInput from './FileInput';
+import FileInput from './index';
 
 describe('<FileInput />', () => {
+    const fn = jest.fn();
+    global.FileReader = function () {};
+    global.FileReader.prototype.readAsText = function (file) {
+        this.onload({
+            target: {
+                result: 'test result',
+            },
+        });
+    };
+
+    const file = shallow(
+        <FileInput
+            name="test"
+            onFileSelect={fn}
+        >
+            test
+        </FileInput>
+    );
+    const input = file.find('input');
+
     it('should render without errors', () => {
-        const fn = jest.fn();
-        const file = shallow(
-            <FileInput
-                name="test"
-                onFileSelect={fn}
-            >
-                test
-            </FileInput>
-        );
         expect(file).toBeDefined();
+    });
+
+    it('should correctly works when file is not selected', () => {
+        const target = {
+            files: [],
+        };
+
+        input.simulate('change', { target });
+        expect(fn).not.toHaveBeenCalled();
+    });
+
+    it('should correctly select file', () => {
+        const target = {
+            files: ['test file'],
+        };
+
+        input.simulate('change', { target });
+        expect(fn).toHaveBeenCalledWith('test file', 'test result');
     });
 });
