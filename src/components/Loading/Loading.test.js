801a3c96eb22a0ee01e29c40288e74f76fbee256
Denis Moiseev <isdenmois@gmail.com>
2017-02-23 00:27:05 +0000

    Rewrite Loading module

--- src/components/Loading/Loading.test.js
+++ src/components/Loading/Loading.test.js
@@ -2,12 +2,12 @@
 import { shallow } from 'enzyme';
 import Loading from './index';
 import React from 'react';
-import { loading } from './Loading.scss';
+import css from './Loading.css';
 
 describe('<Loading />', () => {
     it('should rendering', () => {
         const node = shallow(<Loading />);
         expect(node).toBeDefined();
-        expect(node.hasClass(loading)).toBeTruthy();
+        expect(node.prop('className')).toBe(css.loading);
     });
 });
