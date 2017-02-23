b11c2da40e40d832ede4ac5820d2ded453cae9b3
Denis Moiseev <isdenmois@gmail.com>
2017-02-23 01:12:44 +0000

    Update tests for List component

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -7,8 +7,14 @@ import css from './List.css';
 /* global describe, it, jest, expect */
 describe('<List />', () => {
     it('should render', () => {
-        const list = shallow(<List />);
+        const list = shallow(<List><div /></List>);
         expect(list.length).toBeTruthy();
         expect(list.prop('className')).toBe(css.list);
+    });
+
+    it('should render empty', () => {
+        const list = shallow(<List empty={<span />} />);
+        expect(list.length).toBeTruthy();
+        expect(list.find('span').length).toBe(1);
     });
 });
