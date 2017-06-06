74701a478c40419a3ca3ee7236f05375c1f2f4e1
Denis Moiseev <isdenmois@gmail.com>
2017-06-06 20:55:45 +0300

    Rewrite router with typescript

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import BookPage from 'BookPage/components/BookPage'
+import HomePage from 'HomePage/components/HomePage'
+
+import AppComponent from 'containers/App'
+
+
+export default () => (
+  <AppComponent>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={HomePage}/>
+        <Route path="/book/:MD5" component={BookPage}/>
+      </div>
+    </BrowserRouter>
+  </AppComponent>
+)
